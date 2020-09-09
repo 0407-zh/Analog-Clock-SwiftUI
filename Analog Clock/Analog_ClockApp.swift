1e892fc3a674eb71cbcb0aeb154de35c46640f9c
Derek <chenjunqi07@icloud.com>
2020-09-09 21:25:05 +0800

    Initial Commit

---
+++ Analog Clock/Analog_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Analog_ClockApp.swift
+//  Analog Clock
+//
+//  Created by Derek Chan on 2020/9/9.
+//
+
+import SwiftUI
+
+@main
+struct Analog_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Analog Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Analog Clock
+//
+//  Created by Derek Chan on 2020/9/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
